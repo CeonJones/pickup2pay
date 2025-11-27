561abe6811124ec4d9b3789384ad3e5e0716db6c
ceonjones <ceonsvoice@gmail.com>
2025-11-27 11:46:37 -0600

    types changes

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,11 @@
-export enum CarCondition {
-  EXCELLENT = 'Excellent (Looks new, no issues)',
-  GOOD = 'Good (Minor wear, runs well)',
-  FAIR = 'Fair (Some cosmetic/mechanical issues)',
-  POOR = 'Poor (Not running / Significant damage)'
-}
+export const CarCondition = {
+  EXCELLENT: 'Excellent (Looks new, no issues)',
+  GOOD: 'Good (Minor wear, runs well)',
+  FAIR: 'Fair (Some cosmetic/mechanical issues)',
+  POOR: 'Poor (Not running / Significant damage)'
+} as const;
+
+export type CarCondition = typeof CarCondition[keyof typeof CarCondition];
 
 export interface CarFormData {
   make: string;
